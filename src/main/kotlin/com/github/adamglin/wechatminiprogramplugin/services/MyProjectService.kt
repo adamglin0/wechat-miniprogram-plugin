793bffa94c26604412caaaeae80a338283e483cd
GitHub Action <action@github.com>
2023-10-30 06:20:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/adamglin/wechatminiprogramplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adamglin/wechatminiprogramplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.adamglin.wechatminiprogramplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adamglin.wechatminiprogramplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/adamglin/wechatminiprogramplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/adamglin/wechatminiprogramplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.adamglin.wechatminiprogramplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.adamglin.wechatminiprogramplugin.MyBundle
+import com.github.adamglin.wechatminiprogramplugin.services.MyProjectService
 import javax.swing.JButton
 
 
